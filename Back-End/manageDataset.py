54e36124cf449db5dcbf8accc775407c2fb19077
kderKiro <abdelkadir.megdoud@ensia.edu.dz>
2026-03-31 01:04:43 +0100

    k

---
+++ Back-End/manageDataset.py
@@ -0,0 +1,68 @@
+import requests
+import json
+
+url = "https://api.openchargemap.io/v3/poi/"
+
+params = {
+    "output": "json",
+    "countrycode": "FR",
+    "maxresults": 500,     
+}
+
+headers = {
+    "X-API-Key": "e9bf02f0-1b99-4f5c-bd93-487fe7779998"
+}
+
+response = requests.get(url, params=params, headers=headers)
+
+data = response.json()
+
+with open("charging_stations_raw.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+print("Data saved!")
+
+cleaned_data = []
+
+for station in data:
+    item = {
+        "name": station.get("AddressInfo", {}).get("Title"),
+        "latitude": station.get("AddressInfo", {}).get("Latitude"),
+        "longitude": station.get("AddressInfo", {}).get("Longitude"),
+        "power_kw": None,
+    }
+
+
+    connections = station.get("Connections", [])
+    for conn in connections:
+        if conn.get("PowerKW"):
+            item["power_kw"] = conn["PowerKW"]
+
+    cleaned_data.append(item)
+
+
+with open("charging_stations.json", "w") as f:
+    json.dump(cleaned_data, f, indent=4)
+
+print("Cleaned dataset ready!")
+
+
+vehicle_data = {
+    "name": "Hyundai Ioniq 5",
+    "battery_kwh": 77.4,
+    "range_km": 480,
+    "consumption_kwh_per_km": 77.4 / 480
+}
+
+with open("vehicle.json", "w") as f:
+    json.dump(vehicle_data, f, indent=4)
+
+final_dataset = {
+    "vehicle": vehicle_data,
+    "charging_stations": cleaned_data
+}
+
+with open("ev_dataset.json", "w") as f:
+    json.dump(final_dataset, f, indent=4)
+
+print("Final dataset created!")
--- Front-End/Front-End/src/Map.jsx
+++ Front-End/Front-End/src/Map.jsx
@@ -2,7 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { useState } from 'react';
 import './Map.css'
-import sunIcon from './assets/sun.svg'; 
+import sunIcon from './assets/sun.svg';
 import moonIcon from './assets/moon.png';
 
 export default function Map() {
@@ -11,24 +11,46 @@ export default function Map() {
   const lightTiles = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
   const darkTiles = "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png";
 
+  const [path, setPath] = useState([
+    [36.7115, 2.8425],
+    [36.7189, 2.8525],
+    [36.7215, 2.8625],
+    [36.7315, 2.8725],
+    [36.7415, 2.8825],
+    [36.7535, 2.8912],
+  ]);
+
   return (
     <div className='Container'>
       <button
         onClick={() => setIsDarkMode(!isDarkMode)}
-        
+
       >
         {isDarkMode ? <div className='LightMod'><img src={sunIcon} alt="moon" /> Light</div> : <div className='DarkMod'><img src={moonIcon} alt="moon" /> Dark</div>}
       </button>
 
       <MapContainer
         center={algiersCord}
-        zoom={12}
+        zoom={13}
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url={isDarkMode ? darkTiles : lightTiles}
         />
+
+        <Marker
+          position={[36.7115, 2.8425]}
+        >
+          <Popup>Start Node</Popup>
+        </Marker>
+        <Marker
+          position={[36.7535, 2.8912]}
+        >
+          <Popup>End Node</Popup>
+        </Marker>
+
+        <Polyline positions={path} color="blue" weight={5} opacity={0.7} />
       </MapContainer>
     </div>
   )
